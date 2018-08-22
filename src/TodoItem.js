c0618bb2282f67b38e87e6dd17061900e595cb30
hancong <hancong.guan@mastercard.com>
2018-08-22 16:30:11 +0800

    propTypes & defaultProps

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
     constructor(props) {
@@ -7,10 +8,10 @@ class TodoItem extends Component {
     }
 
 	render() {
-    	const { content } = this.props;
+    	const { content, test } = this.props;
 		return (
 		    <div onClick={this.handleClick}>
-                { content }
+				{ test } - { content }
 		    </div>)
 	}
 
@@ -20,6 +21,17 @@ class TodoItem extends Component {
 		const { deleteItem, index } = this.props;
 		deleteItem(index);
     }
+}
+
+TodoItem.propTypes = {
+	test: PropTypes.string.isRequired,
+	content: PropTypes.string,
+	deleteItem: PropTypes.func,
+	index: PropTypes.number
+}
+
+TodoItem.defaultProps = {
+	test: 'hello world'
 }
 
 export default TodoItem;
